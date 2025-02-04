10e053ae07988a4bdf9f998dcf71924bd3dbc512
Aniket kadukar <aniketdkadukar@gmail.com>
2025-02-05 00:54:42 +0530

    Added requirements.txt

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_core.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    ChatPromptTemplate
+)
+
