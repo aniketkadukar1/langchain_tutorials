2c6bcdc88ad6a12da6aa409ed1e64807646add11
Aniket kadukar <aniketdkadukar@gmail.com>
2025-02-05 23:25:43 +0530

    updated

---
+++ 1_chat_models/1_chat_models_starter.py
@@ -0,0 +1,11 @@
+import getpass
+import os
+from langchain_openai import ChatOpenAI
+
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+model.invoke("Hello World")
